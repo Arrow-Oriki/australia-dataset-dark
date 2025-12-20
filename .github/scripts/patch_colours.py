cb0e93e0e58e11555b522b6ed071ddc5028e8195
Arrow Oriki <admin@whalery.cc>
2025-12-20 13:24:45 +1100

    Improve contrast of PRD Areas

--- .github/scripts/patch_colours.py
+++ .github/scripts/patch_colours.py
@@ -61,7 +61,7 @@ COLOUR_UPDATES = {
     # System2 maps
     ("SecondaryMap", "Alternative System Map"): {"Name": "BlueGrey", "R": "42", "G": "50", "B": "52"},
     # REST_NTZ_DAIW, TDA and Supervisor maps
-    ("PRDArea", "Restricted/Danger Areas"): {"Name": "Red", "R": "53", "G": "4", "B": "4"},
+    ("PRDArea", "Restricted/Danger Areas"): {"Name": "Red", "R": "99", "G": "7", "B": "7"},
     #  Local_Private and Global_Private maps (not currently used)
     ("UserMap", "Custom Maps and Text"): {"Name": "BrightYellow", "R": "235", "G": "235", "B": "0"},
     # Filled maps
